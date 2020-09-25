d07bfea2be54fabc08f24dbc81a02f1fb5d7f3dc
Wagner <Wagner@DESKTOP-838OFJV.local>
2020-09-25 09:26:29 -0300

    Atualização do número da versão.

--- views/login.php
+++ views/login.php
@@ -129,7 +129,7 @@ $logado = $objAccess->logado();
 				  <!-- /.col -->
 				</div>
 				<br>
-				<center><small>Version 1.0.3</small></center>
+				<center><small>Version 1.0.4</small></center>
 			  </form>
 		<!--
 			  <div class="social-auth-links text-center mb-3">
