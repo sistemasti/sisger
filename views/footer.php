5ebdb047bc35aca11e92f0457283074de1da50da
Wagner <wagnerwns@gmail.com>
2020-10-20 09:28:22 -0300

    Merge branch 'master' of https://wagnerns@github.com/sistemasti/sisger.git

--- views/footer.php
+++ views/footer.php
@@ -4,7 +4,7 @@
     <strong><!--CCI Heritage Risk Management Database.--></strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0.7
+      <b>Version</b> 1.0.8
     </div>
   </footer>
 </div>
--- views/login.php
+++ views/login.php
@@ -129,7 +129,7 @@ $logado = $objAccess->logado();
 				  <!-- /.col -->
 				</div>
 				<br>
-				<center><small>Version 1.0.7</small></center>
+				<center><small>Version 1.0.8</small></center>
 			  </form>
 		<!--
 			  <div class="social-auth-links text-center mb-3">
